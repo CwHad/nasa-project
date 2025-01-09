97ffaec07f6ab9b6ba3968c93bad085060b7f3cd
Chad <461755424@qq.com>
2025-01-09 19:03:58 +1300

    fixed

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,17 +1,17 @@
 const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
-const { loadLaunchData } = require("../../models/launches.model");
+const { loadPlanetsData } = require("../../models/planets.model");
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
-    await loadLaunchData();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
     await mongoDisconnect();
-  })
+  });
 
   describe("Test GET /launches", () => {
     test("It should respond with 200 success", async () => {
@@ -24,20 +24,20 @@ describe("Launches API", () => {
 
   describe("Test POST /launch", () => {
     const completeLaunchData = {
-      mission: "USS Enternrise",
+      mission: "USS Enterprise",
       rocket: "NCC 1701-D",
       target: "Kepler-62 f",
       launchDate: "January 4, 2028",
     };
 
     const launchDataWithoutDate = {
-      mission: "USS Enternrise",
+      mission: "USS Enterprise",
       rocket: "NCC 1701-D",
       target: "Kepler-62 f",
     };
 
-    const launchDateWithInvalidDate = {
-      mission: "USS Enternrise",
+    const launchDataWithInvalidDate = {
+      mission: "USS Enterprise",
       rocket: "NCC 1701-D",
       target: "Kepler-62 f",
       launchDate: "zoot",
@@ -68,10 +68,11 @@ describe("Launches API", () => {
         error: "Missing required launch property",
       });
     });
+
     test("It should catch invalid dates", async () => {
       const response = await request(app)
         .post("/v1/launches")
-        .send(launchDateWithInvalidDate)
+        .send(launchDataWithInvalidDate)
         .expect("Content-Type", /json/)
         .expect(400);
 
